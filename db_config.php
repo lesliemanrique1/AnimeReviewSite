9479dbd866c6eddc57c6bb8e879156607cd04a52
lesliemanrique1 <ljm391@nyu.edu>
2016-05-05 10:28:20 -0400

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -4,9 +4,9 @@ ini_set('display_errors', true);
 ini_set('display_startup_errors', true);
 error_reporting(E_ALL);
 
-$db_type           = "mysql";
-$db_server         = "warehouse.cims.nyu.edu";
-$db_name           = "ljm391_A5";
-$db_user           = "ljm391";
-$db_password       = "jkdffsvq";
+$db_type           = "";
+$db_server         = "";
+$db_name           = "";
+$db_user           = "";
+$db_password       = "";
 ?>
